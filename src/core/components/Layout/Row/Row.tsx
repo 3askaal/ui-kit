94356872be7f9b7ded9e4e3d5e31d525e5041d4c
Bas Kaal <3askaal@gmail.com>
2020-07-16 00:54:37 +0200

    fix row component

--- src/core/components/Layout/Row/Row.tsx
+++ src/core/components/Layout/Row/Row.tsx
@@ -2,21 +2,25 @@ import React from 'react'
 import { Stylish } from '../../../utils'
 import { SCol } from '..'
 
-export const SRow = Stylish('div', ({ theme, gutter = 'm' }: any) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'center',
-  width: gutter && `calc(100% + ${theme.space[gutter]})`,
-  marginLeft: gutter && `calc(-${theme.space[gutter]} / 2)`,
-  marginRight: gutter && `calc(-${theme.space[gutter]} / 2)`,
+export const SRow = Stylish(
+  'div',
+  ({ theme, style: { flexWrap = 'nowrap' } = {}, gutter = 'm' }: any) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap,
+    justifyContent: 'center',
+    width: gutter && `calc(100% + ${theme.space[gutter]})`,
+    marginLeft: gutter && `calc(-${theme.space[gutter]} / 2)`,
+    marginRight: gutter && `calc(-${theme.space[gutter]} / 2)`,
 
-  [SCol]: {
-    paddingLeft: gutter && `calc(${theme.space[gutter]} / 2)`,
-    paddingRight: gutter && `calc(${theme.space[gutter]} / 2)`,
-    marginBottom: gutter && `calc(${theme.space[gutter]})`,
-  },
-}))
+    [SCol]: {
+      paddingLeft: gutter && `calc(${theme.space[gutter]} / 2)`,
+      paddingRight: gutter && `calc(${theme.space[gutter]} / 2)`,
+      marginBottom:
+        flexWrap === 'wrap' && gutter && `calc(${theme.space[gutter]})`,
+    },
+  }),
+)
 
 export const Row = (props: any) => {
   return <SRow sRef="Row" {...props} />
