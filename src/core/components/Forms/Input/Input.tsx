05429f42bf42ce908c730ea6a582dc09154b3a80
Bas Kaal <3askaal@gmail.com>
2020-09-27 21:21:12 +0200

    fix input component

--- src/core/components/Forms/Input/Input.tsx
+++ src/core/components/Forms/Input/Input.tsx
@@ -11,6 +11,12 @@ export const Input: FC<any> = ({
   useEffect(() => {
     if (value !== undefined || value !== null) {
       InputRef.current.value = value
+    }
+  }, [])
+
+  useEffect(() => {
+    if (value !== undefined || value !== null) {
+      InputRef.current.value = value
       InputRef.current.focus()
     }
   }, [value])
